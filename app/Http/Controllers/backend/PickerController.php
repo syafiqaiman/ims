a32415e7373260b1765935603a20f9aa069752e3
Syafiq Aiman <syafiqaimanwork1@gmail.com>
2023-08-01 16:51:10 +0800

    Add floor to picker

--- app/Http/Controllers/backend/PickerController.php
+++ app/Http/Controllers/backend/PickerController.php
@@ -8,6 +8,7 @@ use App\Models\Picker;
 use App\Models\Product;
 use App\Models\Order;
 use App\Models\Rack;
+use App\Models\Floor;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
@@ -19,24 +20,34 @@ class PickerController extends Controller
         $this->middleware('auth');
     }
 
-    
+
     public function PickerTaskList()
     {
         $user = Auth::user();
         $pickers = Picker::where('user_id', $user->id)
-                          ->whereIn('status', ['Collected', 'Pending'])
-                          ->orderBy('created_at', 'desc')
-                          ->get();
-    
+            ->whereIn('status', ['Collected', 'Pending'])
+            ->orderBy('created_at', 'desc')
+            ->get();
+
         $allCollected = true; // Set to true by default
         $hasPending = false;
-    
+
         foreach ($pickers as $picker) {
             $product = Product::find($picker->product_id);
             $picker->product_name = $product->name;
             $picker->company_id = $product->company_id;
+
             $rack_location = Rack::where('id', $product->rack_id)->first();
             $picker->location_code = $rack_location->location_code; // Get the location_code from the rack_location
+            
+            $floor_location = Floor::where('id', $product->floor_id)->first();
+            // $picker->location_codes = $floor_location->location_codes; // Get the location_code from the floor_location
+            if ($floor_location) {
+                $picker->location_codes = $floor_location->location_codes; // Get the location_codes from the floor_location if it's not null
+            } else {
+                $picker->location_codes = null; // Set location_codes to null if $floor_location is null
+            }
+
             if ($picker->status !== 'Collected' && $picker->status !== 'Packing') {
                 $allCollected = false; // Set to false if any picker is not collected
                 if ($picker->status === 'Pending') {
@@ -44,88 +55,89 @@ class PickerController extends Controller
                 }
             }
         }
-    
+
         return view('backend.picker.picker_task', [
             'pickers' => $pickers,
             'allCollected' => $allCollected,
             'hasPending' => $hasPending, // Pass the variable to the view
         ]);
     }
-    
-    
-    
-    
 
 
 
-public function confirmCollection(Request $request, $id, $quantity)
-{
-    // Get the picker
-    $picker = Picker::find($id);
-
-    // Update the status to "Collected"
-    $picker->status = "Collected";
-    $picker->save();
-
-    // Find the associated product
-    $product = Product::find($picker->product_id);
-
-    // Create a new order
-    $order = new Order();
-    $order->product()->associate($product);
-    $order->user_id = Auth::user()->id;
-    $order->quantity = $picker->quantity;
-    $order->rack_id = $picker->rack_id;
-    $order->order_no = $picker->order_no;
-    $order->company_id = $product->company_id;
-    $order->save();
-
-    // Redirect back with success message
-    return redirect()->back()->with('success', 'Product collected and order created successfully.');
-}
 
 
-public function history()
-{
-    $user = Auth::user();
-    $pickers = Picker::where('user_id', $user->id)
-                      ->where('status', 'Collected')
-                      ->update(['status' => 'Packing']);
 
-    $orders = Order::with('product')
-                   ->join('products', 'orders.product_id', '=', 'products.id')
-                   ->where('orders.user_id', $user->id) // Retrieve orders of the logged-in picker only
-                   ->orderBy('created_at', 'desc')
-                   ->select('orders.id', 'orders.user_id', 'orders.product_id', 'orders.quantity', 'orders.created_at', 'orders.updated_at', 'products.product_name')
-                   ->get()
-                   ->groupBy(function($order) {
-                        // Group orders by date
-                        return $order->created_at->format('Y-m-d');
-                   });
-                   
-    $dates = $orders->keys(); // Get the dates for the tabs
-    
-    return view('backend.picker.picker_history', [
-        'orders' => $orders,
-        'dates' => $dates,
-    ]);
-}
 
-public function AdminView()
-{
-    $user = Auth::user();
-    
-    $pickers = DB::table('pickers')
-        ->join('users', 'pickers.user_id', '=', 'users.id')
-        ->join('products', 'pickers.product_id', '=', 'products.id')
-        ->select('users.name as picker_name', 'products.product_name', 'pickers.quantity', 'pickers.status', 'pickers.created_at', 'pickers.updated_at')
-        ->groupBy('picker_name', 'products.product_name', 'pickers.quantity', 'pickers.status', 'pickers.created_at', 'pickers.updated_at')
-        ->get();
+    public function confirmCollection(Request $request, $id, $quantity)
+    {
+        // Get the picker
+        $picker = Picker::find($id);
 
-    return view('backend.picker.picker_status_view', [
-        'pickers' => $pickers,
-    ]);
-}
+        // Update the status to "Collected"
+        $picker->status = "Collected";
+        $picker->save();
+
+        // Find the associated product
+        $product = Product::find($picker->product_id);
+
+        // Create a new order
+        $order = new Order();
+        $order->product()->associate($product);
+        $order->user_id = Auth::user()->id;
+        $order->quantity = $picker->quantity;
+        $order->rack_id = $picker->rack_id;
+        $order->floor_id = $picker->floor_id;
+        $order->order_no = $picker->order_no;
+        $order->company_id = $product->company_id;
+        $order->save();
+
+        // Redirect back with success message
+        return redirect()->back()->with('success', 'Product collected and order created successfully.');
+    }
+
+
+    public function history()
+    {
+        $user = Auth::user();
+        $pickers = Picker::where('user_id', $user->id)
+            ->where('status', 'Collected')
+            ->update(['status' => 'Packing']);
+
+        $orders = Order::with('product')
+            ->join('products', 'orders.product_id', '=', 'products.id')
+            ->where('orders.user_id', $user->id) // Retrieve orders of the logged-in picker only
+            ->orderBy('created_at', 'desc')
+            ->select('orders.id', 'orders.user_id', 'orders.product_id', 'orders.quantity', 'orders.created_at', 'orders.updated_at', 'products.product_name')
+            ->get()
+            ->groupBy(function ($order) {
+                // Group orders by date
+                return $order->created_at->format('Y-m-d');
+            });
+
+        $dates = $orders->keys(); // Get the dates for the tabs
+
+        return view('backend.picker.picker_history', [
+            'orders' => $orders,
+            'dates' => $dates,
+        ]);
+    }
+
+    public function AdminView()
+    {
+        $user = Auth::user();
+
+        $pickers = DB::table('pickers')
+            ->join('users', 'pickers.user_id', '=', 'users.id')
+            ->join('products', 'pickers.product_id', '=', 'products.id')
+            ->select('users.name as picker_name', 'products.product_name', 'pickers.quantity', 'pickers.status', 'pickers.created_at', 'pickers.updated_at')
+            ->groupBy('picker_name', 'products.product_name', 'pickers.quantity', 'pickers.status', 'pickers.created_at', 'pickers.updated_at')
+            ->get();
+
+        return view('backend.picker.picker_status_view', [
+            'pickers' => $pickers,
+        ]);
+    }
 
 
 
--- resources/views/backend/picker/picker_task.blade.php
+++ resources/views/backend/picker/picker_task.blade.php
@@ -1,81 +1,85 @@
 @extends('backend.layouts.app')
 
 @section('content')
-<title>Task List</title>
-<div class="container">
-    <div class="row">
-        <div class="col-12">
-            <div class="card">
-                <div class="card-header">
-                    <h3 class="card-title">TASK : Product To Be Collected</h3>
-                    <div class="card-tools">
-                        <div class="input-group input-group-sm" style="width: 150px;">
-                            <input type="text" name="table_search" class="form-control float-right" placeholder="Search">
-                            <div class="input-group-append">
-                                <button type="submit" class="btn btn-default">
-                                    <i class="fas fa-search"></i>
-                                </button>
+    <title>Task List</title>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">TASK : Product To Be Collected</h3>
+                        <div class="card-tools">
+                            <div class="input-group input-group-sm" style="width: 150px;">
+                                <input type="text" name="table_search" class="form-control float-right"
+                                    placeholder="Search">
+                                <div class="input-group-append">
+                                    <button type="submit" class="btn btn-default">
+                                        <i class="fas fa-search"></i>
+                                    </button>
+                                </div>
                             </div>
                         </div>
                     </div>
+                    <div class="card-body table-responsive p-0">
+                        <table class="table table-hover text-nowrap">
+                            <thead>
+                                <tr>
+                                    <th>Rack Location</th>
+                                    <th>Floor Location</th>
+                                    <th>Product Name</th>
+                                    <th>Quantity</th>
+                                    <th>Date of Pick Up</th>
+                                    <th>Status</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($pickers as $picker)
+                                    <tr>
+                                        <td>{{ $picker->location_code }}</td>
+                                        <td>{{ $picker->location_codes }}</td>
+                                        <td>{{ $picker->product->product_name }}</td>
+                                        <td>{{ $picker->quantity }}</td>
+                                        <td>{{ $picker->created_at->format('d M, Y') }}</td>
+                                        <td>
+                                            @if ($picker->status == 'Collected')
+                                                <span class="badge bg-success">{{ $picker->status }}</span>
+                                            @elseif($picker->status == 'Pending')
+                                                <span class="badge bg-warning">{{ $picker->status }}</span>
+                                            @else
+                                                <span class="badge bg-danger">{{ $picker->status }}</span>
+                                            @endif
+                                        </td>
+                                        <td class="status-cell">
+                                            @if ($picker->status == 'Collected')
+                                                <span class="badge bg-success">{{ $picker->status }}</span>
+                                            @elseif($picker->status == 'Pending')
+                                                <form
+                                                    action="{{ route('picker.confirm', ['id' => $picker->id, 'quantity' => $picker->quantity]) }}"
+                                                    method="POST">
+                                                    @csrf
+                                                    <button type="submit"
+                                                        class="btn btn-primary btn-sm btn-collect collect-btn">Collect</button>
+                                                </form>
+                                            @else
+                                                <span class="badge bg-primary">Collected</span>
+                                            @endif
+                                        </td>
+
+
+
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
-                <div class="card-body table-responsive p-0">
-                    <table class="table table-hover text-nowrap">
-                        <thead>
-                            <tr>
-                                <th>Rack Location</th>
-                                <th>Product Name</th>
-                                <th>Quantity</th>
-                                <th>Date of Pick Up</th>
-                                <th>Status</th>
-                                <th>Action</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach($pickers as $picker)
-                            <tr>
-                                <td>{{ $picker->location_code }}</td>
-                                <td>{{ $picker->product->product_name }}</td>
-                                <td>{{ $picker->quantity }}</td>
-                                <td>{{ $picker->created_at->format('d M, Y') }}</td>
-                                <td>
-                                    @if($picker->status == 'Collected')
-                                    <span class="badge bg-success">{{ $picker->status }}</span>
-                                @elseif($picker->status == 'Pending')
-                                    <span class="badge bg-warning">{{ $picker->status }}</span>
-                                @else
-                                    <span class="badge bg-danger">{{ $picker->status }}</span>
-                                @endif
-                                </td>
-                                <td class="status-cell">
-                                @if($picker->status == 'Collected')
-                                    <span class="badge bg-success">{{ $picker->status }}</span>
-                                @elseif($picker->status == 'Pending')
-                                    <form action="{{ route('picker.confirm', ['id' => $picker->id, 'quantity' => $picker->quantity]) }}" method="POST">
-                                        @csrf
-                                        <button type="submit" class="btn btn-primary btn-sm btn-collect collect-btn">Collect</button>
-                                    </form>                                    
-                                @else
-                                    <span class="badge bg-primary">Collected</span>
-                                @endif
-                                </td>
-                                
-                                
-                                
-                            </tr>
-                            @endforeach
-                        </tbody>                        
-                    </table>
+                <div class="text-center mt-4">
+                    <button type="button" class="btn btn-success" id="proceed-to-packing"
+                        @if (count($pickers->where('status', 'Pending'))) disabled @endif>Proceed to Packing</button>
                 </div>
+
             </div>
-            <div class="text-center mt-4">
-                <button type="button" class="btn btn-success" id="proceed-to-packing" @if(count($pickers->where('status', 'Pending'))) disabled @endif>Proceed to Packing</button>
-            </div>
-            
         </div>
     </div>
-</div>
-
-    
-
 @endsection
