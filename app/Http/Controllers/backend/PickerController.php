b97817c76a439895c679844ad5d67ca3152abb9f
Syafiq Aiman <syafiqaimanwork1@gmail.com>
2023-08-04 09:59:47 +0800

    Format

--- app/Http/Controllers/backend/PickerController.php
+++ app/Http/Controllers/backend/PickerController.php
@@ -28,10 +28,10 @@ class PickerController extends Controller
     {
         $user = Auth::user();
         $pickers = Picker::where('user_id', $user->id)
-                          ->whereIn('status', ['Collected', 'Pending', 'Reracking', 'Disposing'])
-                          ->orderBy('created_at', 'desc')
-                          ->get();
-    
+            ->whereIn('status', ['Collected', 'Pending', 'Reracking', 'Disposing'])
+            ->orderBy('created_at', 'desc')
+            ->get();
+
         $allCollected = true; // Set to true by default
         $hasPending = false;
 
@@ -46,17 +46,17 @@ class PickerController extends Controller
 
             $picker->location_code = $rack_location->location_code ?? null; // Get the location_code from the rack_location
             $picker->location_codes = $floor_location->location_codes ?? null; // Get the location_codes from the floor_location
-        
+
             // Access the order_no from the delivery associated with the picker
             $delivery = Delivery::find($picker->order_no);
-    
+
             if ($delivery) {
                 $picker->order_no = $delivery->order_no;
             } else {
                 // Handle case when corresponding delivery record is not found
                 $picker->order_no = 'Not Found'; // Set a placeholder value
             }
-    
+
             if ($picker->status !== 'Collected' && $picker->status !== 'Packing') {
                 $allCollected = false; // Set to false if any picker is not collected
                 if ($picker->status === 'Pending') {
@@ -72,28 +72,23 @@ class PickerController extends Controller
         ]);
     }
 
-
-
-
-
-
     public function confirmCollection(Request $request, $id, $quantity)
     {
         // Get the picker
         $picker = Picker::find($id);
-    
-            // Update the status, report, and remark
+
+        // Update the status, report, and remark
         $picker->status = ($request->report === 'Insufficient' || $request->report === 'Damaged') ? 'Reassign' : 'Collected';
         $picker->report = $request->report;
         $picker->remark = $request->remark;
         $picker->save();
 
-    
+
         // Check if the report is "Completed"
         if ($request->report === 'Completed') {
             // Find the associated product
             $product = Product::find($picker->product_id);
-    
+
             // Create a new order
             $order = new Order();
             $order->product()->associate($product);
@@ -105,71 +100,61 @@ class PickerController extends Controller
             $order->company_id = $product->company_id;
             $order->save();
         }
-    
+
         // Redirect back with success message
         return redirect()->back()->with('success', 'Report updated successfully.');
     }
-    
-    
-
-
 
     public function history()
     {
         $user = Auth::user();
-        
+
         // Update status of pickers
         $pickers = Picker::where('user_id', $user->id)
-                          ->where('status', 'Collected')
-                          ->update(['status' => 'Packing']);
-    
+            ->where('status', 'Collected')
+            ->update(['status' => 'Packing']);
+
         // Retrieve orders and associated delivery order numbers
         $orders = Order::with('product', 'delivery') // Load the 'delivery' relationship
-                       ->join('products', 'orders.product_id', '=', 'products.id')
-                       ->join('delivery', 'orders.order_no', '=', 'delivery.id')
-                       ->where('orders.user_id', $user->id)
-                       ->orderBy('created_at', 'desc')
-                       ->select('orders.id', 'orders.user_id', 'orders.product_id', 'orders.quantity', 'orders.order_no','delivery.order_no', 'orders.created_at', 'orders.updated_at', 'products.product_name')
-                       ->get()
-                       ->groupBy(function($order) {
-                            return $order->created_at->format('d-m-Y');
-                       });
-                       
+            ->join('products', 'orders.product_id', '=', 'products.id')
+            ->join('delivery', 'orders.order_no', '=', 'delivery.id')
+            ->where('orders.user_id', $user->id)
+            ->orderBy('created_at', 'desc')
+            ->select('orders.id', 'orders.user_id', 'orders.product_id', 'orders.quantity', 'orders.order_no', 'delivery.order_no', 'orders.created_at', 'orders.updated_at', 'products.product_name')
+            ->get()
+            ->groupBy(function ($order) {
+                return $order->created_at->format('d-m-Y');
+            });
+
         // Retrieve delivery order numbers for each picker entry
         $pickerOrders = Picker::where('user_id', $user->id)
-                               ->pluck('order_no')
-                               ->toArray();
-    
+            ->pluck('order_no')
+            ->toArray();
+
         $dates = $orders->keys();
-    
+
         return view('backend.picker.picker_history', [
             'orders' => $orders,
             'dates' => $dates,
             'pickerOrders' => $pickerOrders,
         ]);
     }
-    
-    
 
-public function AdminView()
-{
-    $user = Auth::user();
-    
-    $pickers = DB::table('pickers')
-    ->join('users', 'pickers.user_id', '=', 'users.id')
-    ->join('products', 'pickers.product_id', '=', 'products.id')
-    ->select('pickers.id', 'pickers.user_id',  'users.name as picker_name', 'products.product_name', 'pickers.quantity', 'pickers.status', 'pickers.report', 'pickers.remark', 'pickers.created_at', 'pickers.updated_at')
-    ->groupBy('pickers.id', 'pickers.user_id', 'picker_name', 'products.product_name', 'pickers.quantity', 'pickers.status','pickers.report', 'pickers.remark', 'pickers.created_at', 'pickers.updated_at')
-    ->get();
+    public function AdminView()
+    {
+        $user = Auth::user();
+
+        $pickers = DB::table('pickers')
+            ->join('users', 'pickers.user_id', '=', 'users.id')
+            ->join('products', 'pickers.product_id', '=', 'products.id')
+            ->select('pickers.id', 'pickers.user_id', 'users.name as picker_name', 'products.product_name', 'pickers.quantity', 'pickers.status', 'pickers.report', 'pickers.remark', 'pickers.created_at', 'pickers.updated_at')
+            ->groupBy('pickers.id', 'pickers.user_id', 'picker_name', 'products.product_name', 'pickers.quantity', 'pickers.status', 'pickers.report', 'pickers.remark', 'pickers.created_at', 'pickers.updated_at')
+            ->get();
 
 
 
-    return view('backend.picker.picker_status_view', [
-        'pickers' => $pickers,
-    ]);
-}
-
-
-
-
+        return view('backend.picker.picker_status_view', [
+            'pickers' => $pickers,
+        ]);
+    }
 }
